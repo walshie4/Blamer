c4f7fa2a5090b56a05cf8679011c97256c037c09
Adam Walsh <adamwalsh14@gmail.com>
2014-03-25 18:21:58 -0400

    added stripping of all whitespace and rstrip before if statements

--- blamer.py
+++ blamer.py
@@ -5,15 +5,18 @@
 #A simple script to help jailbroken iDevice users find out what
 #Symbolicate finds as the blame for recent springboard crashes
 import os
-os.system("touch /var/mobile/Library/Logs/CrashReporter/blame.txt")
+os.system("touch blame.txt")
 for file in os.listdir("/var/mobile/Library/Logs/CrashReporter"):
     if file.endswith(".plist"):
         output = os.popen("symbolicate " + file).read()
         recording = False #true when writing to output file, false otherwise
         for line in output:
+            line = "".join(line.rstrip.split)
             if recording:
                 open("blame.txt","a").write(line)
-            if line.rstrip() == "<key>blame</key>": #starting of what we're looking for
+            if line == "<key>blame</key>": #starting of what we're looking for
+                print("recording")
                 recording = True
-            if line.rstrip() == "<key>symbolicated</key>":
+            if line == "<key>symbolicated</key>":
+                print("stopping")
                 recording = False
