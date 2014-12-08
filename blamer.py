ac5bcb8ca3dbe03654d5b866cbc8fde279933719
Adam Walsh <adamwalsh14@gmail.com>
2014-12-08 00:57:15 -0500

    Updated for new sybolicate update

--- blamer.py
+++ blamer.py
@@ -3,7 +3,7 @@
 #Written on 3/25/14
 
 #A simple script to help jailbroken iDevice users find out what
-#Symbolicate finds as the blame for recent springboard crashes
+#Symbolicate finds as the blame for recent crashes
 import os
 location = "/var/mobile/Library/Logs/CrashReporter/"
 if os.path.isfile(location + "blame.txt"):
@@ -13,29 +13,20 @@ os.system("touch " + location + "temp.txt")
 path = "/var/mobile/Library/Logs/CrashReporter/"
 crashes = dict() #dictionary to hold all found crashes
 for file in os.listdir(path):
-    if file.endswith(".plist"):
+    if file.endswith(".ips"):
         print("now reading: " + file)
-        command = "symbolicate " + path + file + " > " + location + "temp.txt"
+        command = "symbolicate --print-blame " + path + file + " > " + location + "temp.txt"
         os.system(command)
         recording = False #true when writing to output file, false otherwise
         output = open(location + "temp.txt")
         for line in output:
-            line = "".join(line.rstrip().split())
-            if recording:
-                for word in line.split(' '):
-                    if word.startswith("<array><string>"):
-                        word = word[15:] #cut off XML tags at beginning
-                        word = word.split('<')[0] #grab only the path
-                        if word in crashes: #already been found
-                            crashes[word] += 1 #increment its count
-                        else:
-                            crashes[word] = 1 #add to dict
-            if line == "<key>blame</key>": #starting of what we're looking for
-                print("recording")
-                recording = True
-            if line == "<key>symbolicated</key>":
-                print("stopping")
-                recording = False
+            if line.startswith("/"):#is a path
+                word = line.rsplit("/")
+                if word in crashes: #already been found
+                    crashes[word] += 1 #increment its count
+                else:
+                    crashes[word] = 1 #add to dict
 for element in sorted(crashes, key=crashes.get, reverse=True):
     open(location + "blame.txt","a").write(element + " - " + str(crashes[element]) + "\n")
 os.system("rm " + location + "temp.txt")
+
